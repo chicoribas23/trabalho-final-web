18895ff0763e8b8450802afcc79eb1bd6d9ae9b3
kaikygomes86767 <kgl2@aluno.ifnmg.edu.br>
2025-10-22 20:22:37 +0000

    Mensagem de commmit

--- db/DDL.sql
+++ db/DDL.sql
@@ -1,15 +1,12 @@
--- ==========================
--- BANCO DE DADOS: VIKINGS ACADEMIA
--- ==========================
-
-CREATE TABLE usuarios (
+CREATE TABLE IF NOT EXISTS usuarios (
     id_usuario SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE planos (
+-- Criar tabela de planos
+CREATE TABLE IF NOT EXISTS planos (
     id_plano SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     descricao TEXT,
@@ -17,7 +14,8 @@ CREATE TABLE planos (
     id_usuario INT REFERENCES usuarios(id_usuario)
 );
 
-CREATE TABLE fotos (
+-- Criar tabela de fotos
+CREATE TABLE IF NOT EXISTS fotos (
     id_foto SERIAL PRIMARY KEY,
     titulo VARCHAR(100),
     caminho_arquivo VARCHAR(255),
