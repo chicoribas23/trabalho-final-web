99109b29b1a65d45aa4cd8fc91df54284a0408c1
kaikygomes86767 <kgl2@aluno.ifnmg.edu.br>
2025-12-13 15:08:53 +0000

    rr

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const botaoAutor = document.getElementById('botaoAutor');
+const elAutor = document.getElementById('autor');
+const elApi = document.getElementById('api');
+const elBd = document.getElementById('bd');
+
+botaoAutor.addEventListener('click', pegarInformacao);
+
+const urlBase = "https://back-end-tf-web-i3bt.vercel.app/";
+
+async function pegarInformacao() {
+
+  elAutor.innerText = "Aguarde... "
+
+  try {
+    const response = await fetch(urlBase);
+
+    if (!response.ok) {
+      throw new Error("Erro na requisição: " + response.status);
+    }
+
+    const data = await response.json();
+    elAutor.innerHTML = `<strong>Autor:</strong> ${data.autor}`;
+    elApi.innerHTML = `<strong>API:</strong> ${data.mensagem}`;
+    elBd.innerHTML = `<strong>Banco de Dados:</strong> ${data.dbStatus}`;
+
+  } catch (error) {
+    console.error("Erro:", error);
+    elAutor.innerText = `Erro: ${error}`;
+  }
+}
